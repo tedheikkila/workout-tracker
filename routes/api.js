078472dd17598dfa31a6e2577e80e84ce16e0c2f
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-02 16:58:27 -0500

    more setup

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,28 @@
-db.Workout.create({ type: "Resistance" })
-  .then(dbWorkout => {
-    console.log(dbWorkout);
-  })
-  .catch(({message}) => {
-    console.log(message);
-  });
+const router = require("express").Router();
+const Exercise = require("../models/Exercise.js");
 
-  db.Workout.create({ type: "Cardio" })
-  .then(dbWorkout => {
-    console.log(dbWorkout);
-  })
-  .catch(({message}) => {
-    console.log(message);
-  });
 
-app.post("/submit", ({body}, res) => {
-  db.Exercise.create(body)
-    .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+router.post("/api/workouts", ({ body }, res) => {
+  Exercise.create(body)
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
     })
     .catch(err => {
-      res.json(err);
+      res.status(400).json(err);
     });
 });
 
 
 
 
+
+// app.post("/submit", ({body}, res) => {
+//   db.Exercise.create(body)
+//     .then(({_id}) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+//     .then(dbWorkout => {
+//       res.json(dbWorkout);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
 
+const db = require("./models");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -14,9 +16,25 @@ const config = { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", config);
 
+db.Workout.create({ type: "Resistance" })
+    .then(dbWorkout => {
+        console.log(dbWorkout);
+    })
+    .catch(({ message }) => {
+        console.log(message);
+    });
+
+db.Workout.create({ type: "Cardio" })
+    .then(dbWorkout => {
+        console.log(dbWorkout);
+    })
+    .catch(({ message }) => {
+        console.log(message);
+    });
+
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on ${PORT}`);
+    console.log(`App running on ${PORT}`);
 });
