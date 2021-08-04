c3d086be146041b0cdff7777969803bc63ab6ada
Ted Heikkila <ted.heikkila@gmail.com>
2021-08-03 19:38:37 -0500

    laid basework for api rts, tweak later

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,61 @@
 const router = require("express").Router();
 const Workout = require("../models/Workout.js");
+const db = require("../models")
 
+//gets last workout (tweak)
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+  .sort({ date: 1 })
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch (err => {
+    res.json(err)
+  })
+})
 
+//gets workouts in range (tweak; what's range)
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch (err => {
+    res.json(err)
+  })
+})
+
+//gets a workout by its id (might not need later)
+router.get("/api/workouts/:id", (req, res) => {
+  db.Workout.findById(id)
+  .then(dbWorkout => {
+    res.json(dbWorkout)
+  })
+  .catch (err => {
+    res.json(err)
+  })
+})
+
+// creates new workout in db (should be fine)
 router.post("/api/workouts", ({ body }, res) => {
-  Workout.create(body)
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
+  db.Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
+    });
+});
+
+// updates a workout w/an exercise (should be fine)
+router.put("/api/workouts/:id", ({ body }, res) => {
+  db.Workout.create(body)
+    .then(({ _id }) => db.Workout.findOneAndUpdate({}, { $push: { exercises: _id } }, { new: true }))
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
     });
 });
 
